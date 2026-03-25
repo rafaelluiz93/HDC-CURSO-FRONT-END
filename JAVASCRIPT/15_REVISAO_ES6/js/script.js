63c57f24d772f718f8bc1b86fdc33bf2971a5a31
rafaelluiz93 <rafa.luiz1993@gmail.com>
2026-03-24 21:27:30 -0300

    Revisão ES6: filter | map

--- JAVASCRIPT/15_REVISAO_ES6/js/script.js
+++ JAVASCRIPT/15_REVISAO_ES6/js/script.js
@@ -83,7 +83,49 @@ const user = {
 user.sayUserName()
 user.sayUserNameArrow()
 
+
+
+// FILTER
+
+//LISTA 1
+const arr = [1, 2, 3, 4, 5, 6]
+
+const highNumbers = arr.filter((n) => {
+    if(n >= 3) {
+        return n
+    }
+})
+
+console.log(highNumbers)
+
+//LISTA 2
+const users = [
+    {name: "Rafael", available: true},
+    {name: "Eduardo", available: false},
+    {name: "Aline", available: true},
+    {name: "Leandro", available: true},
+    {name: "Letícia", available: false}
+]
+
+const availableUsers = users.filter((user) => user.available)
+console.log(availableUsers)
+
+
+//MAP
+const products = [
+    {name: "Camisa", price: 15.00, category: "Roupas"},
+    {name: "Torradeira", price: 80.00, category: "Eletro"},
+    {name: "Fogão", price: 700.00, category: "Eletro"},
+    {name: "Calça Jeans", price: 90.00, category: "Roupas"}
+]
+
+products.map((product) => {
+    if(product.category === "Roupas"){
+        product.onSale = true
+    }
+})
+
+console.log(products)
+
 */
-
-
 
